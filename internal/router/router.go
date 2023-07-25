391d3a701de60c26397414236c05af102148f2ff
botingchen <Brad8910@gmail.com>
2023-07-26 01:46:03 +0800

    update dbghost api routing to dbg

--- internal/router/router.go
+++ internal/router/router.go
@@ -24,14 +24,17 @@ func Start_backend() {
 	Port := config.Viper.GetString("SERVER_PORT")
 
 	// GET request
+	//kvm
 	router.GET("/api/kvm/list", kvm_api.Kvm_list)
 	router.GET("/api/kvm/info", kvm_api.Kvm_info)
 	router.GET("/api/kvm/search", kvm_api.Kvm_search)
 
-	router.GET("/api/dbghost/list", dbghost_api.Dbghost_list)
-	router.GET("/api/dbghost/info", dbghost_api.Dbghost_info)
-	router.GET("/api/dbghost/search", dbghost_api.Dbghost_search)
+	//debug_host
+	router.GET("/api/dbg/list", dbghost_api.Dbghost_list)
+	router.GET("/api/dbg/info", dbghost_api.Dbghost_info)
+	router.GET("/api/dbg/search", dbghost_api.Dbghost_search)
 
+	//dut
 	router.GET("/api/dut/list", dut_api.Dut_list)
 	router.GET("/api/dut/info", dut_api.Dut_info)
 	router.GET("/api/dut/search", dut_api.Dut_search)
